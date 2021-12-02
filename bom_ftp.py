eb27e9f171f7da0e4a5e83a2b016885200c2f664
Ian McHugh <ian.mchugh@unimelb.edu.au>
2021-12-02 11:27:20 +1100

    Added function for the retrieval of charts from BOM

---
+++ bom_ftp.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 15:45:32 2021
+
+@author: imchugh
+"""
+
+import ftplib
+import pathlib
+import sys
+
+#------------------------------------------------------------------------------
+### Constants
+#------------------------------------------------------------------------------
+DIRS_DICT = {'satellite': 'anon/gen/gms',
+             'mslp': 'anon/gen/difacs'}
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+### Classes
+#------------------------------------------------------------------------------
+
+class bom_ftp_getter():
+
+    """Get ftp chart file lists from bom ftp server and get files"""
+
+    def __init__(self, img_type):
+
+        try:
+            self.img_type = DIRS_DICT[img_type]
+        except KeyError as e:
+            raise Exception('img_type must be one of: {}'
+                            .format(', '.join(list(DIRS_DICT.keys())))) from e
+        bom_ftp = 'ftp.bom.gov.au'
+        ftp = ftplib.FTP(bom_ftp)
+        ftp.login()
+        self.ftp = ftp
+
+    def get_file_list(self, file_type=None, search_str=None):
+
+        files = self.ftp.nlst(self.img_type)
+        if file_type:
+            files = [x for x in files if file_type in x]
+        if search_str:
+            files = [x for x in files if search_str in x]
+        return files
+
+    def get_file(self, src_file, target_dir):
+
+        out_file = pathlib.Path(target_dir) / pathlib.Path(src_file).name
+        with open(out_file, 'wb') as f:
+            self.ftp.retrbinary('RETR {}'.format(src_file), f.write)
+
+if __name__ == "__main__":
+
+    out_path = sys.argv[1]
+    getter = bom_ftp_getter(img_type='satellite')
+    file_list = getter.get_file_list(file_type='jpg', search_str='IDE00135.20')
+    getter.get_file(src_file=file_list[-1], target_dir=out_path)
+    getter = bom_ftp_getter(img_type='mslp')
+    getter.get_file(src_file='anon/gen/difacs/IDX0894.gif',
+                    target_dir=out_path)
