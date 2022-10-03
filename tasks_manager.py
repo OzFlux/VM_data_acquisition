a61e7b67e3cea1bccae108ee17ba0d98c3220c9d
Ian McHugh <imchugh@unimelb.edu.au>
2022-10-03 15:45:36 +1000

    Added tasks manager to consolidate processes under one Python roof

---
+++ tasks_manager.py
@@ -0,0 +1,325 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 11:29:27 2022
+
+This code coordinates and executes the tasks to be run across sites, including 
+a batch function that can be run for all sites for which the given task is 
+enabled. This list is specified in a spreadsheet (Tasks tab) that resides on 
+Cloudstor.
+
+@author: jcutern-imchugh
+"""
+
+#------------------------------------------------------------------------------
+### STANDARD IMPORTS ###
+#------------------------------------------------------------------------------
+
+import logging
+from logging import handlers
+import pandas as pd
+import pathlib
+import subprocess as spc
+import sys
+import pdb
+
+#------------------------------------------------------------------------------
+### CUSTOM IMPORTS ###
+#------------------------------------------------------------------------------
+
+import campbell_funcs as cf
+import file_merger as fm
+import paths_manager as pm
+import process_10hz_data as ptd
+
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+### INITIALISATION ###
+#------------------------------------------------------------------------------
+
+# Import the paths object to get designated paths to data streams
+PATHS = pm.paths()
+
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+### FUNCTIONS ###
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def _set_logger(site, task):
+
+    """Create logger and send output to file"""
+
+    logger_write_path = (
+        PATHS.get_local_path(resource='logs', site=site) / f'{site}_{task}.txt'
+        )
+    this_logger = logging.getLogger()
+    this_logger.setLevel(logging.INFO)
+    handler = logging.handlers.RotatingFileHandler(logger_write_path, 
+                                                   maxBytes=10**6)
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s [%(name)s] %(message)s'
+        )
+    handler.setFormatter(formatter)
+    this_logger.addHandler(handler)
+    return this_logger
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+### CLASSES ###
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+class tasks_manager():
+    
+    def __init__(self):
+
+        self.tasks_df = self._make_df()
+
+    def _make_df(self):
+        """
+        Construct the dataframe containing listed tasks to be executed for each 
+        site in the collection - read from the spreadsheet (Tasks tab).
+
+        Returns
+        -------
+        pd.core.Frame.DataFrame
+            Dataframe containing the tasks that need to be executed for each 
+            site.
+
+        """
+        
+        return pd.read_excel(
+            PATHS.variable_map(), sheet_name='Tasks', index_col='Site'
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def generate_merged_file(self, site):
+        """
+        Pull data from downloaded tables and merge into single table with 
+        standard names for variables. Used for RTMC plotting.
+
+        Parameters
+        ----------
+        site : str
+            Site name.
+
+        """
+        
+        merger = fm.file_merger(site=site)
+        output_path = (
+            PATHS.get_local_path(
+                resource='data', stream='flux_slow', site=site
+                ) 
+            / '{}_merged_std.dat'.format(site)
+            )
+        merger.make_output_file(dest=output_path)
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def generate_site_details_file(self, site):
+        """
+        Create the file containing the site information in dummy TOA5 format,
+        and write to the appropriate directory.
+
+        Parameters
+        ----------
+        site : str
+            Site name.
+
+        """
+        
+        output_path = (
+            PATHS.get_local_path(resource='site_details') / 
+            '{}_details.dat'.format(site)
+            )
+        maker = cf.make_site_info_TOA5(site=site, num_to_str=['Start year'])
+        maker.output_file(output_path)
+
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------    
+    def get_site_list_for_task(self, task_string):
+        """
+        Get the list of sites for which a given task is enabled.
+        """
+        
+        return self.tasks_df.loc[self.tasks_df[task_string]].index.tolist()
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------    
+    def get_task_list(self):
+        """
+        Get the list of tasks.
+        """
+        
+        return self.tasks_df.columns.tolist()
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------            
+    def rclone_push_data(self, site, stream):
+        """
+        Push data from local to Cloudstor folders.
+
+        Parameters
+        ----------
+        site : str
+            Site name.
+        stream : str
+            The data stream to push (limited to flux_slow, flux_fast and 
+                                     RTMC).
+
+        """
+        
+        logging.info(f'Begin push of data stream "{stream}" for site "{site}"')
+        exclude_dict = {'flux_fast': 'TMP'}
+        to_remote = PATHS.get_remote_path(
+            resource='cloudstor', stream=stream, site=site
+            )
+        from_local = PATHS.get_local_path(
+            resource='data', stream=stream, site=site, check_exists=True
+            )
+        exclude_dirs = [] if not stream in exclude_dict else exclude_dict[stream]
+        self._rclone_push_pull_generic(
+            source_dir=str(from_local), target_dir=str(to_remote),
+            exclude_dirs=exclude_dirs
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def rclone_pull_data(self, site):
+        """
+        Pull data from Cloudstor to local folders. Currently limited to slow
+        data.
+
+        Parameters
+        ----------
+        site : str
+            Site name.
+        stream : str
+            The data stream to pull (limited to flux_slow, flux_fast and 
+                                     RTMC).
+
+        """
+        
+        stream = 'flux_slow'
+        from_remote = PATHS.get_remote_path(
+            resource='cloudstor', stream=stream, site=site
+            )
+        to_local = PATHS.get_local_path(
+            resource='data', stream=stream, site=site, check_exists=True
+            )     
+        self._rclone_push_pull_generic(
+            source_dir=str(from_remote), target_dir=str(to_local),
+            )
+    #--------------------------------------------------------------------------
+    
+    #--------------------------------------------------------------------------
+    def reformat_10Hz_data(self, site):
+        
+            pass
+    #--------------------------------------------------------------------------
+    
+    #--------------------------------------------------------------------------
+    def run_task(self, site, task_string):
+        """
+        Run a task from the task list.
+
+        Parameters
+        ----------
+        site : str
+            Site name.
+        task_string : str
+            The name of the task to run.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        configs_dict = {
+            'generate_merged_file': {
+                'function': self.generate_merged_file, 'stream': None
+                },
+            'generate_site_details_file': {
+                'function': self.generate_site_details_file, 'stream': None
+                },
+            'Rclone_push_slow': {
+                'function': self.rclone_push_data, 'stream': 'flux_slow'
+                },
+            'Rclone_pull_slow': {
+                'function': self.rclone_pull_data, 'stream': 'flux_slow'
+                },
+            'Rclone_push_fast': {
+                'function': self.rclone_push_data, 'stream': 'flux_fast'
+                },
+            'Rclone_push_RTMC': {
+                'function': self.rclone_push_data, 'stream': 'RTMC'
+                },
+            'Reformat_10Hz': {
+                'function': self.reformat_10Hz_data, 'stream': None
+                },
+            }
+        
+        task_function = configs_dict[task_string]['function']
+        task_stream = configs_dict[task_string]['stream']
+        if task_stream:
+            task_function(site=site, stream=task_stream)
+        else:
+            task_function(site=site)
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def _rclone_push_pull_generic(self, source_dir, target_dir, exclude_dirs):
+
+        app_str = PATHS.get_application_path(
+            application='rclone', as_str=True, check_exists=False
+            )
+        if 'cloudstor' in target_dir:
+            rslt = spc.run([app_str, 'lsd', target_dir], capture_output=True)
+            rslt.check_returncode()        
+        exec_list = [
+            'copy', '--transfers', '36', '--progress', '--checksum',
+            '--checkers', '48', '--timeout', '0'
+            ]
+        if exclude_dirs:
+            for this_dir in exclude_dirs:
+                exec_list.append('--exclude')
+                exec_list.append(str(pathlib.Path('{}/**'.format(this_dir))))
+        exec_list = [app_str] + exec_list + [source_dir, target_dir]
+        rslt = spc.run(exec_list, capture_output=True)
+        rslt.check_returncode()
+    #--------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+### MAIN PROGRAM ###
+#------------------------------------------------------------------------------
+
+# Main function
+def main():
+    
+    tasks = tasks_manager()
+    task = sys.argv[1]
+    try:
+        site = sys.argv[2]
+        logger = _set_logger(site=site, task=task)
+        logger.info(f'Running task "{task}" for site {site}')
+        tasks.run_task(site=site, task_string=task)
+        logger.handlers.clear()
+    except IndexError:
+        site_list = tasks.get_site_list_for_task(task_string=task)
+        for site in site_list:
+            logger = _set_logger(site=site, task=task)
+            logger.info(f'Running task "{task}" for site {site}')
+            tasks.run_task(site=site, task_string=task)    
+            logger.info('Done')
+            logger.handlers.clear()
+
+if __name__=='__main__':
+    
+    main()
