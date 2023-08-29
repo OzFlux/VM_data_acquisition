e59204d785b9dd23d68a1abea13e8f5414300809
Ian McHugh <imchugh@unimelb.edu.au>
2023-08-29 16:46:56 +1000

    Upgraded functionality

--- rtmc_xml_parser.py
+++ rtmc_xml_parser.py
@@ -622,3 +622,383 @@ class rtmc_parser():
             raise FileExistsError('No overwrite of template file allowed!')
         self.tree.write(str(file_name_fmt))
     #--------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+class RtmcSyntaxGenerator():
+
+    def __init__(self):
+
+        pass
+
+    #--------------------------------------------------------------------------
+    def _get_init_dict(self, start_cond):
+        """
+        Get the requested RTMC-formatted start condition.
+
+        Parameters
+        ----------
+        start_cond : str
+            The start condition required.
+
+        Returns
+        -------
+        dict
+            A dictionary with key 'start_cond' and the RTMC start condition
+            string as value.
+
+        """
+
+        start_dict = {
+            'start': 'StartRelativeToNewest({},OrderCollected);',
+            'start_absolute': 'StartAtRecord(0,0,OrderCollected);'
+            }
+        if not start_cond:
+            return {}
+        return {'start_cond': start_dict[start_cond]}
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def _get_cumulative_total(
+            self, eval_string, timestamp_var, scaling_str=None, interval='daily'
+            ):
+
+        if not scaling_str:
+            scaling_str = ''
+        interval_dict = {
+            'daily': 'RESET_DAILY'
+            }
+        return self._str_joiner([
+            'TotalOverTimeWithReset(',
+            '(',
+            f'{eval_string}',
+            f'){scaling_str},',
+            f'Timestamp({timestamp_var}),',
+            f'{interval_dict[interval]}',
+            ')'
+            ],
+            joiner='\n'
+            )
+
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def _get_scaled_to_range(self, eval_string):
+        """
+        Scale an RTMC evaluated string relative to its range.
+
+        Parameters
+        ----------
+        eval_string : str
+            The string that will be evaluated by RTMC.
+
+        Returns
+        -------
+        str
+            RTMC-readable string to generate a variable scaled relative to its
+            range (max - min).
+
+        """
+
+        return (
+            f'({eval_string} - MinRun({eval_string})) / '
+            f'(MaxRun({eval_string}) - MinRun({eval_string}))'
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_alias_string(self, var_list):
+        """
+        Generate an RTMC-valid alias structure.
+
+        Parameters
+        ----------
+        long_name : str
+            The variable for which to return the alias string.
+
+        Returns
+        -------
+        str
+            Formatted RTMC alias string.
+
+        """
+
+        return self._str_joiner(
+            [f'Alias({x},"DataFile:merged.{x}");' for x in var_list],
+            joiner='\r\n'
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_no_data_status_string(self, logger_name, table_name):
+
+        return f'"Server:{logger_name}.{table_name}"'
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_comm_status_string(self, logger_name):
+        """
+
+
+        Parameters
+        ----------
+        logger_name : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+
+        return (
+            f'"Server:__statistics__.{logger_name}_std.Collection State" > 2 '
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_aliased_output(
+            self, var_list, as_str=True, start_cond=None,
+            scaled_to_range=False
+            ):
+
+        alias_string = self.get_alias_string(var_list=var_list)
+        eval_string = ','.join(var_list)
+        if len(var_list) > 1:
+            eval_string = f'AvgSpa({eval_string})'
+        if scaled_to_range:
+            eval_string = self._get_scaled_to_range(eval_string=eval_string)
+            start_cond = 'start_absolute'
+        strings_dict = self._get_init_dict(start_cond=start_cond)
+        strings_dict.update(
+            {'alias_string': alias_string, 'eval_string': eval_string}
+            )
+        if as_str:
+            return self._str_joiner(list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_net_radiation(self, cuml=False, as_str=True):
+
+        strings_dict = {
+            'alias_string': self._str_joiner(str_list=
+                [self.get_aliased_output(var_list=[var], as_str=False)['alias_string']
+                 for var in ['Fsd', 'Fsu', 'Fld', 'Flu']
+                 ],
+                joiner='\r\n'
+                ),
+            'eval_string': 'Fsd-Fsu+Fld-Flu'
+            }
+        if cuml:
+            strings_dict['eval_string'] = (
+                self._get_cumulative_total(
+                    eval_string=strings_dict['eval_string'],
+                    timestamp_var='Fsd',
+                    scaling_str='*1800/10^6'
+                    )
+                )
+        if as_str:
+            return self._str_joiner(str_list=list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_net_turbulent_energy(self, as_str=True):
+
+        strings_dict = {
+            'alias_string': self._str_joiner(str_list=
+                [self.get_aliased_output(var_list=[var], as_str=False)['alias_string']
+                 for var in ['Fh', 'Fe']
+                 ],
+                joiner='\r\n'
+                ),
+            'eval_string': 'Fh+Fe'
+            }
+        if as_str:
+            return self._str_joiner(str_list=list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_net_non_radiant_energy(
+            self, soil_HF_list, soil_T_list=None, cuml=False, as_str=True,
+            ):
+
+        turbulent_flux = self.get_net_turbulent_energy(as_str=False)
+        if soil_T_list:
+            soil_flux = self.get_corrected_soil_heat_flux(
+                soil_HF_list=soil_HF_list, soil_T_list=soil_T_list,
+                as_str=False
+                )
+        else:
+            soil_flux = self.get_aliased_output(
+                var_list=soil_HF_list,
+                as_str=False
+                )
+        strings_dict = {
+            'alias_string': self._str_joiner(
+                [turbulent_flux['alias_string'], soil_flux['alias_string']],
+                joiner='\r\n'
+                ),
+            'eval_string': self._str_joiner([
+                f'{turbulent_flux["eval_string"]}+',
+                '(',
+                f'{soil_flux["eval_string"]}',
+                ')'
+                ],
+                joiner='\n'
+                )
+            }
+        if cuml:
+            strings_dict['eval_string'] = (
+                self._get_cumulative_total(
+                    eval_string=strings_dict['eval_string'],
+                    timestamp_var='Fh',
+                    scaling_str='*1800/10^6'
+                    )
+                )
+        if as_str:
+            return self._str_joiner(list(strings_dict.values()))
+        return strings_dict
+        pass
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_energy_balance_residual(
+            self, soil_HF_list, soil_T_list=None, as_str=True
+            ):
+
+        radiant_flux = self.get_net_radiation(as_str=False)
+        nonradiant_flux = self.get_net_non_radiant_energy(
+            soil_HF_list=soil_HF_list, soil_T_list=soil_T_list, as_str=False
+            )
+        strings_dict = {
+            'alias_string': self._str_joiner(
+                [radiant_flux['alias_string'], nonradiant_flux['alias_string']],
+                joiner='\r\n'
+                ),
+            'eval_string': self._str_joiner([
+                f'{radiant_flux["eval_string"]}-',
+                '(',
+                f'{nonradiant_flux["eval_string"]}',
+                ')'
+                ],
+                joiner='\n'
+                )
+            }
+        if as_str:
+            return self._str_joiner(list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_available_energy(self, soil_HF_list, soil_T_list=None, as_str=True):
+
+        net_rad = self.get_net_radiation(as_str=False)
+        if soil_T_list:
+            soil_flux = self.get_corrected_soil_heat_flux(
+                soil_HF_list=soil_HF_list, soil_T_list=soil_T_list,
+                as_str=False
+                )
+        else:
+            soil_flux = self.get_soil_heat_flux(
+                soil_HF_list=soil_HF_list,
+                as_str=False
+                )
+        strings_dict = {
+            'alias_string': self._str_joiner(
+                [net_rad['alias_string'], soil_flux['alias_string']],
+                joiner='\r\n'
+                ),
+            'eval_string': self._str_joiner([
+                f'({net_rad["eval_string"]})-',
+                '(',
+                f'{soil_flux["eval_string"]}',
+                ')'
+                ],
+                joiner='\n'
+                )
+            }
+        if as_str:
+            return self._str_joiner(list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_soil_heat_storage(
+            self, soil_T_list, Cp=1800, seconds=1800, layer_depth=0.08,
+            as_str=True, start_cond=None
+            ):
+
+        avg_dict = (
+            self.get_aliased_output(var_list=soil_T_list, as_str=False)
+            )
+        alias_string = self._str_joiner(
+            [avg_dict['alias_string'], f'Alias(Cp,{Cp});'],
+            joiner='\r\n'
+            )
+        eval_string = self._str_joiner([
+            'Cp*(',
+            f'{avg_dict["eval_string"]}-',
+            f'Last({avg_dict["eval_string"]})',
+            f')/({seconds}*{layer_depth})'
+            ],
+            joiner='\n'
+            )
+        strings_dict = self._get_init_dict(start_cond=start_cond)
+        strings_dict.update(
+            {'alias_string': alias_string, 'eval_string': eval_string}
+            )
+        if as_str:
+            return self._str_joiner(list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_corrected_soil_heat_flux(
+            self, soil_HF_list, soil_T_list, Cp=1800, seconds=1800,
+            layer_depth=0.08, as_str=True
+            ):
+
+        stor_dict = self.get_soil_heat_storage(soil_T_list=soil_T_list,
+            Cp=Cp, seconds=seconds, layer_depth=layer_depth, as_str=False
+            )
+        flux_dict = self.get_soil_heat_flux(
+            soil_HF_list=soil_HF_list, as_str=False
+            )
+        strings_dict = {
+            'alias_string': self._str_joiner(
+                str_list=[flux_dict['alias_string'], stor_dict['alias_string']],
+                joiner='\r\n'
+                ),
+            'eval_string': self._str_joiner([
+                f'{flux_dict["eval_string"]}+',
+                f'{stor_dict["eval_string"]}',
+                ],
+                joiner='\n'
+                )
+            }
+        if as_str:
+            return self._str_joiner(list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def get_soil_heat_flux(self, soil_HF_list, as_str=True):
+
+        strings_dict = self.get_aliased_output(
+            var_list=soil_HF_list, as_str=False
+            )
+        if as_str:
+            return self._str_joiner(list(strings_dict.values()))
+        return strings_dict
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def _str_joiner(self, str_list, joiner='\r\n\r\n'):
+
+        return joiner.join(str_list)
+    #--------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
