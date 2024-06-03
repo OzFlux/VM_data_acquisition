bcc7ab810806d8a39d439f0dad13afa0d5ea788a
Ian McHugh <imchugh@unimelb.edu.au>
2024-06-03 16:07:38 +1000

    Added lots of functionality, need to test append functions and add safety checks for merge

--- nc_constructor.py
+++ nc_constructor.py
@@ -2,19 +2,21 @@
 """
 Created on Thu May 16 16:25:29 2024
 
+Todo:
+    - test mode 'a' for appending data
+
 @author: jcutern-imchugh
 """
 
 import datetime as dt
 import pandas as pd
+import pathlib
 import sys
 import xarray as xr
 
 import config_getters as cg
 import file_handler as fh
 import metadata_handlers as mh
-from sparql_site_details import site_details
-
 
 #------------------------------------------------------------------------------
 SITE_DETAIL_ALIASES = {'elevation': 'altitude'}
@@ -28,16 +30,55 @@ VAR_METADATA_SUBSET = [
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 #------------------------------------------------------------------------------
 
+###############################################################################
+### BEGIN DATA CONSTRUCTOR CLASS ###
+###############################################################################
+
 #------------------------------------------------------------------------------
-class NetCDFBuilder():
+class L1DataBuilder():
 
     #--------------------------------------------------------------------------
-    def __init__(self, site):
+    def __init__(self, site, md_mngr=None, concat_files=False):
 
         self.site = site
-        self.md_mngr = mh.MetaDataManager(site=site)
-        self.data = merge_data_by_manager(site=site, md_mngr=self.md_mngr)
-        self.global_attrs = get_site_global_attrs(site=site)
+        if md_mngr is None:
+            md_mngr = mh.MetaDataManager(site=site)
+        self.md_mngr = md_mngr
+        self.data = merge_data_by_manager(
+            site=site, md_mngr=self.md_mngr, concat_files=concat_files
+            )
+        self.data_years = self.data.index.year.unique().tolist()
+        self.global_attrs = self._get_site_global_attrs()
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def _get_site_global_attrs(self) -> dict:
+        """
+
+
+        Args:
+            site (TYPE): DESCRIPTION.
+
+        Returns:
+            TYPE: DESCRIPTION.
+
+        """
+
+        global_attrs = cg.get_nc_global_attrs()
+        new_dict = {
+            'metadata_link':
+                global_attrs['metadata_link'].replace('<site>', self.site),
+            'site_name': self.site
+            }
+        global_attrs.update(new_dict)
+
+        site_specific_attrs = (
+            self.md_mngr.site_details
+            [SITE_DETAIL_SUBSET]
+            .rename(SITE_DETAIL_ALIASES)
+            .to_dict()
+            )
+        return global_attrs | site_specific_attrs
     #--------------------------------------------------------------------------
 
     #--------------------------------------------------------------------------
@@ -74,26 +115,115 @@ class NetCDFBuilder():
     #--------------------------------------------------------------------------
 
     #--------------------------------------------------------------------------
-    def build_xarray_dataset(self, year: int=None) -> xr.Dataset:
+    def build_xarray_dataset_complete(self) -> xr.Dataset:
+        """
+        Build an xarray dataset constructed from all of the raw data.
+
+        Returns:
+            The dataset.
+
+        """
+
+        return self._build_xarray_dataset(df=self.data)
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def build_xarray_dataset_by_slice(
+            self, start_date: dt.datetime | str=None,
+            end_date: dt.datetime | str=None
+            ) -> xr.Dataset:
+        """
+        Build an xarray dataset constructed from a discrete time slice.
+
+        Args:
+            start_date (optional): the start date for the data. If None,
+            starts at the beginning of the merged dataset. Defaults to None.
+            end_date (optional): the end date for the data. If None,
+            ends at the end of the merged dataset. Defaults to None.
+
+        Returns:
+            The dataset.
+
+        """
+
+        if start_date is None and end_date is None:
+            return self.build_xarray_dataset_complete()
+        if start_date is None:
+            return self._build_xarray_dataset(
+                df=self.data.loc[: end_date]
+                )
+        if end_date is None:
+            return self._build_xarray_dataset(
+                df=self.data.loc[start_date:]
+                )
+        return self._build_xarray_dataset(
+            df=self.data.loc[start_date: end_date]
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def build_xarray_dataset_by_year(self, year: int) -> xr.Dataset:
+
+        """
+        Build an xarray dataset constructed from a given year.
+
+        Args:
+            year: the data year to return.
+
+        Returns:
+            The dataset.
+
+        """
+
+        if not year in self.data_years:
+            years = ', '.join([str(year) for year in self.data_years])
+            raise IndexError(
+                f'Data year is not available (available years: {years})'
+                )
+        bounds = self._get_year_bounds(
+            year=year,
+            time_step=self.global_attrs['time_step']
+            )
+        return self._build_xarray_dataset(
+            df=self.data.loc[bounds[0]: bounds[1]]
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def _get_year_bounds(self, year: int, time_step: int) -> dict:
+        """
+
+
+        Args:
+            year (int): DESCRIPTION.
+            time_step (int): DESCRIPTION.
+
+        Returns:
+            dict: DESCRIPTION.
+
+        """
+
+        return (
+                (
+                (dt.datetime(year, 1, 1) + dt.timedelta(minutes=time_step))
+                .strftime(TIME_FORMAT)
+                ),
+            dt.datetime(year + 1, 1, 1).strftime(TIME_FORMAT)
+            )
+    #--------------------------------------------------------------------------
+
+    #--------------------------------------------------------------------------
+    def _build_xarray_dataset(self, df: pd.core.frame.DataFrame) -> xr.Dataset:
         """
         Convert the dataframe to an xarray dataset and apply global attributes.
 
         Args:
-            year (optional): year for which to subset data.
+            df: the dataframe to convert to xr dataset.
 
         Returns:
             ds: xarray dataset.
 
         """
-
-        # Subset the dataset by year (or not)
-        df = self.data.copy()
-        if year is not None:
-            bounds = get_year_bounds(
-                year=year,
-                time_step=self.global_attrs['time_step']
-                )
-            df = df.loc[bounds[0]: bounds[1]]
 
         # Create xarray dataset
         ds = (
@@ -112,6 +242,7 @@ class NetCDFBuilder():
         self._set_dim_encoding(ds=ds)
         self._assign_variable_attrs(ds=ds)
         self._assign_variable_flags(ds=ds)
+
         return ds
     #--------------------------------------------------------------------------
 
@@ -210,57 +341,147 @@ class NetCDFBuilder():
         name_list = [self.site]
         if not year is None:
             name_list.append(str(year))
-        name_list.append('L1.nc')
+
         path = self.md_mngr.data_path / f'{"_".join(name_list)}'
         ds.to_netcdf(path=path, mode='w')
     #--------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
-def get_year_bounds(year: int, time_step: int) -> dict:
+###############################################################################
+### END DATA CONSTRUCTOR CLASS ###
+###############################################################################
 
-    return (
-            (
-            (dt.datetime(year, 1, 1) + dt.timedelta(minutes=time_step))
-            .strftime(TIME_FORMAT)
-            ),
-        dt.datetime(year + 1, 1, 1).strftime(TIME_FORMAT)
-        )
-#------------------------------------------------------------------------------
+
+
+###############################################################################
+### BEGIN NC WRITERS ###
+###############################################################################
 
 #------------------------------------------------------------------------------
-def get_site_global_attrs(site: str) -> dict:
+def make_nc_file(site: str, split_by_year: bool=True):
     """
-
+    Merge all data and metadata sources to create a netcdf file.
 
     Args:
-        site (TYPE): DESCRIPTION.
+        site: name of site.
+        split_by_year (optional): write discrete year files. Defaults to True.
 
     Returns:
-        TYPE: DESCRIPTION.
+        None.
 
     """
 
-    global_attrs = cg.get_nc_global_attrs()
-    new_dict = {
-        'metadata_link': global_attrs['metadata_link'].replace('<site>', site),
-        'site_name': site
-        }
-    global_attrs.update(new_dict)
-
-    site_specific_attrs = (
-        site_details().get_single_site_details(site=site)
-        [SITE_DETAIL_SUBSET]
-        .rename(SITE_DETAIL_ALIASES)
-        .to_dict()
-        )
-    return global_attrs | site_specific_attrs
+    data_builder = L1DataBuilder(site=site, concat_files=True)
+    if not split_by_year:
+        ds = data_builder.build_xarray_dataset_complete()
+        output_path = data_builder.md_mngr.data_path / f'{site}_L1.nc'
+        ds.to_netcdf(path=output_path, mode='w')
+        return
+    for year in data_builder.data_years:
+        ds = data_builder.build_xarray_dataset_by_year(year=year)
+        output_path = data_builder.md_mngr.data_path / f'{site}_{year}_L1.nc'
+        ds.to_netcdf(path=output_path, mode='w')
 #------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def make_nc_year_file(site: str, year: int):
+    """
+    Merge all data and metadata sources to create a single year netcdf file.
+
+    Args:
+        site: name of site.
+        year: year to write.
+
+    Raises:
+        IndexError: raised if dataset does not contain passed year.
+
+    Returns:
+        None.
+
+    """
+
+    data_builder = L1DataBuilder(site=site, concat_files=True)
+    if not year in data_builder.data_years:
+        raise IndexError('No data available for current data year!')
+    ds = data_builder.build_xarray_dataset_by_year(year=year)
+    output_path = data_builder.md_mngr.data_path / f'{site}_{year}_L1.nc'
+    ds.to_netcdf(path=output_path, mode='w')
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def append_to_current_nc_file(site: str):
+    """
+    Check for current year nc file, and if exists, append. If not, create.
+
+    Args:
+        site: name of site.
+
+    Returns:
+        None.
+
+    """
+
+    md_mngr = mh.MetaDataManager(site=site)
+    expected_year = dt.datetime.now().year
+    expected_file = md_mngr.data_path / f'{site}_{expected_year}_L1.nc'
+    if not expected_file.exists:
+        make_nc_year_file(site=site, year=expected_year)
+    else:
+        append_to_nc(site=site, nc_file=expected_file)
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+def append_to_nc(site: str, nc_file: pathlib.Path | str):
+    """
+    Generic append function. Only appends new data.
+
+    Args:
+        site: name of site.
+        nc_file: nc file to which to append data.
+
+    Returns:
+        None.
+
+    """
+
+    ds = xr.open_dataset(nc_file)
+    last_nc_date = pd.Timestamp(ds.time.values[-1]).to_pydatetime()
+    md_mngr = mh.MetaDataManager(site=site)
+    last_raw_date = min(
+        [md_mngr.get_file_attrs(x)['end_date'] for x in md_mngr.list_files()]
+        )
+    if not last_raw_date > last_nc_date:
+        print('No new data to append!')
+        return
+    data_builder = L1DataBuilder(site=site, md_mngr=md_mngr)
+    date_iloc = data_builder.data.index.get_loc(last_nc_date) + 1
+    new_ds = data_builder.build_xarray_dataset_by_slice(
+        start_date=data_builder.data.index[date_iloc]
+        )
+    combined_ds = xr.concat([ds, new_ds], dim='time', combine_attrs='override')
+    ds.close()
+    combined_ds.attrs['time_coverage_end'] = (
+        pd.to_datetime(combined_ds.time.values[-1])
+        .strftime(TIME_FORMAT)
+        )
+    combined_ds.attrs['nc_nrecs'] = len(combined_ds.time)
+    combined_ds.to_netcdf(path=nc_file, mode='w')
+#------------------------------------------------------------------------------
+
+###############################################################################
+### END NC WRITERS ###
+###############################################################################
+
+
+
+###############################################################################
+### BEGIN MERGE FUNCTIONS ###
+###############################################################################
 
 #------------------------------------------------------------------------------
 def merge_data_by_manager(
-        site: str, md_mngr: mh.MetaDataManager=None
+        site: str, md_mngr: mh.MetaDataManager=None, concat_files=False
         ) -> pd.core.frame.DataFrame:
     """
 
@@ -280,28 +501,9 @@ def merge_data_by_manager(
         file: md_mngr.translate_variables_by_table(table=table)
         for table, file in md_mngr.map_tables_to_files(abs_path=True).items()
         }
-    return fh.merge_data(files=merge_dict)
+    return fh.merge_data(files=merge_dict, concat_files=concat_files)
 #------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
-def make_nc(site, year=None):
-
-    nc_builder = NetCDFBuilder(site=site)
-    nc_builder.write_to_netcdf(year=year)
-#------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------
-def append_to_nc():
-
-    pass
-#------------------------------------------------------------------------------
-
-# #------------------------------------------------------------------------------
-# if __name__ == '__main__':
-
-#     site = sys.argv[1]
-
-#     make_dataset(site=sys.argv[1])
-
-
-# #------------------------------------------------------------------------------
+###############################################################################
+### END MERGE FUNCTIONS ###
+###############################################################################
